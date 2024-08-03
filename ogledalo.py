41f14793491a53d86a9ef01ae95c2d30b52db4b0
Luka Lavš <lavsluka@gmail.com>
2024-08-03 12:20:31 +0200

    new file:   ogledalo.py

---
+++ ogledalo.py
@@ -0,0 +1,36 @@
+def ogledalo_vip(koef, d, tocka, vektor):
+    
+    zelja = tocka
+    odboji=0
+    while True:
+        if math.sqrt(sum([(a-b)**2 for (a,b) in zip(tocka, zelja)])) < 0.101:
+            if odboji != 0:
+                odboji-=1
+                break
+            
+        a = sum([k*v**2 for v,k in zip(vektor,koef)])
+        b = sum([k*2*v*b for v,b,k in zip(vektor,tocka,koef)])
+        c = sum([k*b**2 for b,k in zip(tocka,koef)]) - d
+        t1 = ( -b + (b**2-4*a*c)**0.5 )/(2*a)
+        t2 = ( -b - (b**2-4*a*c)**0.5 )/(2*a)
+   
+        if abs(t1)<0.0001:
+            tocka =  [t2*v+b for v,b in zip(vektor,tocka)]
+        else:
+            tocka = [t1*v+b for v,b in zip(vektor,tocka)] 
+        
+        gradient = [2*k*x for x,k in zip(tocka,koef)]
+        proj = ( sum([v*g for v,g in zip(vektor,gradient)])/sum([g**2 for g in gradient]) )
+        proj = [proj*g for g in gradient]
+            
+        vektor = [2*p - v for p,v in zip(proj, vektor)]
+        print(f"t:{[round(t,3) for t in tocka]}, v: {[round(v,3) for v in vektor]}")
+               
+        odboji+=1   
+        
+    return odboji
+    
+#print(ogledalo_vip([4,2], 100, [0,7.071],[-1.4,-19.7]))       
+
+#print(ogledalo_vip([4,1,0.5], 1, [0,0,2**0.5],[-1,-2,-1]))       
+
